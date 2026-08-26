bb6d17ba10b8573447caa122a55a4b869cd889a9
manavrawat1247 <rawatmanav4213@gmail.com>
2026-08-26 23:14:16 +0530

    ...

---
+++ classroom coding/day17 code 2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main() 
+{
+    int num,c=0,i;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    for(i=1;i<=num;i++)
+    {
+        if(num%i==0)
+        {
+            c++;
+        }
+    }
+    if(c==2)
+    {
+        printf("%d is a prime number.\n", num);
+    }
+    else
+    {
+        printf("%d is not a prime number.\n", num);
+    }
+    return 0;
+}
