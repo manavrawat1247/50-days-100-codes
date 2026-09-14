c7d9af88a83f5063d07aa69dcb53cf3cc8703a3a
manavrawat1247 <rawatmanav4213@gmail.com>
2026-09-14 13:40:33 +0530

    ..

---
+++ classroom coding/day36 code2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int i,j,n,m;
+printf("Enter rows:");
+scanf("%d",&n);
+printf("Enter column");
+scanf("%d",&m);
+arr[n][m];
+printf("Enter array elements");
+for(i=0;i<=n-1;i++)
+{
+for(j=0;j<=m-1;j++)
+{
+scanf("%d",&arr[i][j]);
+sum=arr[i][j];
+}
+}
